302ebe3049ca24d29191e87323a36daf2ac3b4c2
Louis-Mulders <louis.mulders@openvantage.co.za>
2019-10-25 11:10:17 +0200

    .

--- src/Events/Events.js
+++ src/Events/Events.js
@@ -11,7 +11,7 @@ import Event from '../Event/Event';
 import styles, { CONTENT_OFFSET } from './Events.styles';
 
 const { width: screenWidth } = Dimensions.get('window');
-const TIME_LABELS_COUNT = 8;
+const TIME_LABELS_COUNT = 7;
 const MINUTES_IN_HOUR = 60;
 const MINUTES_IN_DAY = MINUTES_IN_HOUR * 24;
 const ROW_HEIGHT = 40;
@@ -66,7 +66,7 @@ class Events extends Component {
     const startHours = moment(item.startDate).hours();
     const startMinutes = moment(item.startDate).minutes();
     const totalStartMinutes = (startHours * MINUTES_IN_HOUR) + startMinutes;
-    const topOffset = (totalStartMinutes * CONTENT_HEIGHT) / MINUTES_IN_DAY;
+    const topOffset = (totalStartMinutes * 48) / MINUTES_IN_DAY;
     const height = (moment(item.endDate).diff(item.startDate, 'minutes') * 48) / MINUTES_IN_DAY;
     const width = this.getEventItemWidth();
 
